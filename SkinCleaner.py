9c7b7052cdc0601c0197afd55986a67074061ff0
Sigen_Chang <97452067+Ryumo0205@users.noreply.github.com>
2022-08-31 19:12:13 +0800

    判別式更新

--- SkinCleaner.py
+++ SkinCleaner.py
@@ -29,13 +29,14 @@ def check_vtx(x_IQRscale,y_IQRscale,z_IQRscale,InfluencesName=str):
     """
     使用迴圈填入參數,能一次遍歷所有骨架影響
     """
-    if x_IQRscale < 1.0 or x_IQRscale > 30:
+    print("Go!")
+    if x_IQRscale < 1.0 or x_IQRscale > 10:
         pm.select(clear=True)
         return
-    if y_IQRscale < 1.0 or y_IQRscale > 30:
+    if y_IQRscale < 1.0 or y_IQRscale > 10:
         pm.select(clear=True)
         return
-    if z_IQRscale < 1.0 or z_IQRscale > 30:
+    if z_IQRscale < 1.0 or z_IQRscale > 10:
         pm.select(clear=True)
         return
 
@@ -54,6 +55,10 @@ def check_vtx(x_IQRscale,y_IQRscale,z_IQRscale,InfluencesName=str):
     inf_vtx = pm.filterExpand(sm=31)
     if inf_vtx == None:
         return
+
+    inf_vtx_len = len(inf_vtx)
+    print("inf_vtx_len:",inf_vtx_len)
+
 
     #取得受影響的vtx的xyz座標
     for i in inf_vtx:
@@ -75,21 +80,26 @@ def check_vtx(x_IQRscale,y_IQRscale,z_IQRscale,InfluencesName=str):
     for qz in range(3):
         qz = qz + 1
         globals()["z_Q"+str(qz)] = quantile_exc(z_list, qz)
-    # #計算離群值
+    
+    #計算四分位距
     x_IQR = x_Q3 - x_Q1
     y_IQR = y_Q3 - y_Q1
     z_IQR = z_Q3 - z_Q1
+    print("x_Q3:",x_Q3,"x_Q1:",x_Q1)
+    print("y_Q3:",y_Q3,"y_Q1:",y_Q1)
+    print("z_Q3:",z_Q3,"z_Q1:",z_Q1)
+
     print("x_IQR:",x_IQR)
     print("y_IQR:",y_IQR)
     print("z_IQR:",z_IQR)
 
-    ######為求精準此處應加入Q1Q3的象限判別#######
-    x_IQR_max = (x_Q3 + x_IQR) * x_IQRscale
-    x_IQR_min = (x_Q1 - x_IQR) * (1.0 / x_IQRscale)
-    y_IQR_max = (y_Q3 + y_IQR) * y_IQRscale
-    y_IQR_min = (y_Q1 - y_IQR) * (1.0 / y_IQRscale)
-    z_IQR_max = (z_Q3 + z_IQR) * z_IQRscale
-    z_IQR_min = (z_Q1 - z_IQR) * (1.0 / z_IQRscale)
+    #計算離群值乘上倍率
+    x_IQR_max = (x_Q3 + (x_IQR * x_IQRscale))
+    x_IQR_min = (x_Q1 - (x_IQR * x_IQRscale))
+    y_IQR_max = (y_Q3 + (y_IQR * y_IQRscale))
+    y_IQR_min = (y_Q1 - (y_IQR * y_IQRscale))
+    z_IQR_max = (z_Q3 + (z_IQR * z_IQRscale))
+    z_IQR_min = (z_Q1 - (z_IQR * z_IQRscale))
 
     print("x_outliyer:",x_IQR_max,"|",x_IQR_min)
     print("y_outliyer:",y_IQR_max,"|",y_IQR_min)
@@ -116,22 +126,86 @@ def check_vtx(x_IQRscale,y_IQRscale,z_IQRscale,InfluencesName=str):
             pass
 
     #合併xyz名單,刪除重複的vtx,返回一個被檢測出有問題的vtx的list
+    print(x_fix_vtx)
+    print(y_fix_vtx)
+    print(z_fix_vtx)
     fix_vtx = set(x_fix_vtx + y_fix_vtx + z_fix_vtx)
     fix_vtx = list(fix_vtx)
-    return fix_vtx
+    return fix_vtx , inf_vtx_len
 #-----------------------------------------------------------------------------------#
+checked_vtx_list = []
 
-
-#fixed_vtx = check_vtx(1.3,1.5,1.0,"L_Arm")
-#執行方法,得到有問題的vtx的list
 for one_inf in inf_list:
-    print(one_inf)
-    fixed_vtx = check_vtx(1.3,1.5,1.0,one_inf)
-    print(one_inf, fixed_vtx)
+    checked_data = check_vtx(1.0, 1.0, 1.0, one_inf)
+    if checked_data == None :
+        temp_list = [one_inf,None]
+        checked_vtx_list.append(temp_list)
+        continue
+    else:
+        fix_vtx = checked_data[0]
+        data_len = checked_data[1]
+        print(len(fix_vtx) / float(data_len))
+
+        x_att , y_att ,z_att = 1.0, 1.0 ,1.0
+
+        while True:
+            if data_len < 3 :   #####這邊有問題,不能用點來判斷會除不盡
+                temp_list = [one_inf,None]
+                checked_vtx_list.append(temp_list)
+                break
+            else:
+                pass
+            if len(fix_vtx) / float(data_len) > 0.01 :
+                x_att += 0.2
+                y_att += 0.2
+                z_att += 0.2
+                checked_data = check_vtx(x_att, y_att, z_att, one_inf)
+                fix_vtx = checked_data[0]
+                data_len = checked_data[1]
+                # print(len(fix_vtx))
+                # print(data_len)
+                print(len(fix_vtx) / float(data_len))
+                print("smooth!")
+            else:
+                break
+        if fix_vtx == [] :
+            temp_list = [one_inf,None]
+            checked_vtx_list.append(temp_list)
+        else:
+            globals()[one_inf+"_vtx_list"] = fix_vtx
+            temp_list = [one_inf,globals()[one_inf+"_vtx_list"]]
+            checked_vtx_list.append(temp_list)
+
+print("OK!")
+print(L_Arm_list)
+
+for iq in checked_vtx_list:
+    if iq[1] == None :
+        pass
+    else:
+        print(iq)
+# checked_data = check_vtx(1.0, 1.0, 1.0, "L_Arm")
+# fix_vtx = checked_data[0]
+# data_len = checked_data[1]
+
+# print(len(fix_vtx))
+# print(data_len)
+# print(len(fix_vtx) / float(data_len))
+
+
+
+# 執行方法,得到有問題的vtx的list
+# for one_inf in inf_list:
+#     print(one_inf)
+#     checked_data = check_vtx(1.3,1.5,1.0,one_inf) 
+#     print(one_inf, checked_data)
+
+
+
 
 #選擇並顯示有問題的點
-if fixed_vtx ==  None:
-    pm.warning("IQRscale must 1.0 ~ 2.0 .")
-else:
-    pass
-    #pm.select(fixed_vtx)
+# if fix_vtx ==  None:
+#     pm.warning("IQRscale must 1.0 ~ 2.0 .")
+# else:
+#     pass
+#     pm.select(fix_vtx)
